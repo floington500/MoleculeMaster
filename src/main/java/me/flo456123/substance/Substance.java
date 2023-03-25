6bbbf0adc13b423a9da4acb7d26a922a6eac441e
flo456123 <zachappella@gmail.com>
2023-03-25 12:32:15 -0400

    added substance exception and implemented it in code

--- src/main/java/me/flo456123/substance/Substance.java
+++ src/main/java/me/flo456123/substance/Substance.java
@@ -3,10 +3,10 @@ package me.flo456123.substance;
 import me.flo456123.substance.reactants.Reactant;
 
 public class Substance {
-    private final double   nMoles;
-    private final Reactant reactant;
-    private final double   mass;
-    private final Unit     unit;
+    private double   nMoles;
+    private Reactant reactant;
+    private double   mass;
+    private Unit     unit;
 
     /**
      * Substance stores a reactant along with its mass, unit, and moles
@@ -16,30 +16,50 @@ public class Substance {
      * @param unit what unit the substance is
      */
     public Substance(double n, Reactant r, double mass, Unit unit) {
-        if (mass < 0) {
-            throw new RuntimeException("invalid mass - mass cannot be negative");
-        }
-
-        nMoles = n;
-        reactant = r;
-        this.mass = mass;
-        this.unit = unit;
+        setnMoles(n);
+        setReactant(r);
+        setMass(mass);
+        setUnit(unit);
     }
 
     public double getMoles() {
         return nMoles;
     }
 
+    private void setnMoles(double n) {
+        if (n < 0) {
+            throw new SubstanceException("invalid number of moles - moles cannot be negative");
+        }
+
+        nMoles = n;
+    }
+
     public Reactant getReactant() {
         return reactant;
+    }
+
+    private void setReactant(Reactant reactant) {
+        this.reactant = reactant;
     }
 
     public double getMass() {
         return mass;
     }
 
+    private void setMass(double mass) {
+        if (mass < 0) {
+            throw new SubstanceException("invalid mass - mass cannot be negative");
+        }
+
+        this.mass = mass;
+    }
+
     public Unit getUnit() {
         return unit;
+    }
+
+    private void setUnit(Unit unit) {
+        this.unit = unit;
     }
 
 }
---
+++ src/main/java/me/flo456123/substance/SubstanceException.java
@@ -0,0 +1,9 @@
+package me.flo456123.substance;
+
+public class SubstanceException extends RuntimeException {
+
+    public SubstanceException(String msg) {
+        super(msg);
+    }
+
+}
