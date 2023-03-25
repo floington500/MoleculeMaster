320f8d84db0ef9c347ed9b7373babd1379bc66a0
flo456123 <zachappella@gmail.com>
2023-03-25 11:23:06 -0400

    added element framework

--- src/main/java/me/flo456123/element/Element.java
+++ src/main/java/me/flo456123/element/Element.java
@@ -1,4 +1,77 @@
 package me.flo456123.element;
 
-public class Element {
+public abstract class Element {
+    private static ElementType elementType;
+    private static byte        atomicNumber;
+    private static String      elementSymbol;
+    private static double      atomicMass;
+    private int                atoms;
+
+    public Element(int atoms) {
+        this.setAtoms(atoms);
+    }
+
+    public int getAtoms() {
+        return atoms;
+    }
+
+    protected void setAtoms(int atoms) {
+        this.atoms = atoms;
+    }
+
+    public ElementType getElementType() {
+        return elementType;
+    }
+
+    protected static void setElementType(ElementType type) {
+        elementType = type;
+    }
+
+    public byte getAtomicNumber() {
+        return atomicNumber;
+    }
+
+    protected static void setAtomicNumber(byte number) {
+        if (number < 1 || number > 118) {
+            throw new RuntimeException("invalid atomic number");
+        }
+
+        atomicNumber = number;
+    }
+
+    public String getElementSymbol() {
+        return elementSymbol;
+    }
+
+    protected static void setElementSymbol(String symbol) {
+        if (symbol.length() > 2) {
+            throw new RuntimeException("invalid element symbol");
+        }
+
+        elementSymbol = symbol;
+    }
+
+    public double getAtomicMass() {
+        return atomicMass;
+    }
+
+    protected static void setAtomicMass(double mass) {
+        atomicMass = mass;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder(getElementSymbol() + " ")
+                .append(getAtoms())
+                .append(" ")
+                .append(getAtomicMass())
+                .append(" ")
+                .append(getElementType())
+                .append(" ")
+                .append(getAtomicNumber())
+                .append(" ")
+                .append(getAtomicMass())
+                .toString();
+    }
+
 }
---
+++ src/main/java/me/flo456123/element/ElementType.java
@@ -0,0 +1,5 @@
+package me.flo456123.element;
+
+public enum ElementType {
+    IONIC, COVALENT
+}
