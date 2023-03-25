8d8caec122914af55b4d17d471a298e14ca2db2d
flo456123 <zachappella@gmail.com>
2023-03-25 15:59:40 -0400

    created element factory for parser

---
+++ src/main/java/me/flo456123/element/ElementFactory.java
@@ -0,0 +1,29 @@
+package me.flo456123.element;
+
+import me.flo456123.element.elements.*;
+
+public class ElementFactory {
+    public static Element createElement(String symbol, int atoms) {
+        return switch (symbol) {
+            case "Al" -> new Aluminum(atoms);
+            case "Ar" -> new Argon(atoms);
+            case "Be" -> new Beryllium(atoms);
+            case "B" -> new Boron(atoms);
+            case "C" -> new Carbon(atoms);
+            case "Cl" -> new Chlorine(atoms);
+            case "F" -> new Fluorine(atoms);
+            case "He" -> new Helium(atoms);
+            case "H" -> new Hydrogen(atoms);
+            case "Li" -> new Lithium(atoms);
+            case "Mg" -> new Magnesium(atoms);
+            case "Ne" -> new Neon(atoms);
+            case "N" -> new Nitrogen(atoms);
+            case "O" -> new Oxygen(atoms);
+            case "P" -> new Phosphorus(atoms);
+            case "Si" -> new Silicon(atoms);
+            case "Na" -> new Sodium(atoms);
+            case "S" -> new Sulfur(atoms);
+            default -> throw new ElementException("invalid element symbol");
+        };
+    }
+}
