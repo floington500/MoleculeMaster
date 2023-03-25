990476555515d55e8a90ee710dced42f27ba5cf3
flo456123 <zachappella@gmail.com>
2023-03-25 16:55:29 -0400

    added datatype for chemical equation and (empty) parser

---
+++ src/main/java/me/flo456123/utils/ChemicalEquation.java
@@ -0,0 +1,34 @@
+package me.flo456123.utils;
+
+import me.flo456123.substance.Substance;
+
+import java.util.List;
+
+public class ChemicalEquation {
+    private List<Substance> reactants;
+    private List<Substance> products;
+
+    public ChemicalEquation(String equation) {
+        String[] equationSides = equation.split("->");
+        String[] reactantStrings = equationSides[0].split("\\+");
+        String[] productStrings = equationSides[1].split("\\+");
+
+        for (String reactantString : reactantStrings) {
+            reactants.add(Parser.parseSubstanceString(reactantString));
+        }
+
+        for (String productString : productStrings) {
+            products.add(Parser.parseSubstanceString(productString));
+        }
+
+    }
+
+    public List<Substance> getReactants() {
+        return reactants;
+    }
+
+    public List<Substance> getProducts() {
+        return products;
+    }
+
+}
---
+++ src/main/java/me/flo456123/utils/Parser.java
@@ -0,0 +1,10 @@
+package me.flo456123.utils;
+
+import me.flo456123.substance.Substance;
+
+public class Parser {
+    public static Substance parseSubstanceString(String substanceString) {
+        // TODO: implement substance string parsing
+        return null;
+    }
+}
