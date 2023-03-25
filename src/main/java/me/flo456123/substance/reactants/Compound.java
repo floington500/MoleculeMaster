caa2e90ab9c81ae52b458cc54a80dcb7e28b4c12
flo456123 <zachappella@gmail.com>
2023-03-25 11:52:34 -0400

    added some reactants

---
+++ src/main/java/me/flo456123/substance/reactants/Compound.java
@@ -0,0 +1,11 @@
+package me.flo456123.substance.reactants;
+
+import me.flo456123.element.Element;
+
+public class Compound extends Reactant {
+
+    public Compound(Element e1, Element e2) {
+        elements = new Element[]{e1, e2};
+    }
+
+}
---
+++ src/main/java/me/flo456123/substance/reactants/Molecule.java
@@ -0,0 +1,11 @@
+package me.flo456123.substance.reactants;
+
+import me.flo456123.element.Element;
+
+public class Molecule extends Reactant {
+
+    public Molecule(Element element) {
+        elements = new Element[]{element};
+    }
+
+}
