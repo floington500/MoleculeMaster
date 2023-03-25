885589d3fed0b7a21874c4e3f0eeb46c88a631a0
flo456123 <zachappella@gmail.com>
2023-03-25 12:34:13 -0400

    made mini program to decide my lunch because i cant decide myself

--- src/main/java/me/flo456123/Main.java
+++ src/main/java/me/flo456123/Main.java
@@ -1,7 +1,15 @@
 package me.flo456123;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int choice = new Random().nextInt(2);
+        if (choice == 0) {
+            System.out.println("Burrito");
+        }
+        else {
+            System.out.println("Burrito bowl");
+        }
     }
 }
