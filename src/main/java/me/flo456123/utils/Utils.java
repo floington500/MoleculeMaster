bc90c1507421417edb262d789a54eff758151cd7
flo456123 <zachappella@gmail.com>
2023-03-25 13:18:13 -0400

    created utils.java... not excited for this part of the project :(

---
+++ src/main/java/me/flo456123/utils/Utils.java
@@ -0,0 +1,11 @@
+package me.flo456123.utils;
+
+//TODO stuff to add
+// convert between reactants and products
+// parse chemical equation
+// method to convert between units
+// convert between reactants
+
+public class Utils {
+
+}
