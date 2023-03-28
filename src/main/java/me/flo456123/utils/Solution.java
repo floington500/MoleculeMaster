d6c44e398c8c502cbd804d6b003b2c3100f06f80
flo456123 <zachappella@gmail.com>
2023-03-27 21:21:10 -0400

    made solution class

---
+++ src/main/java/me/flo456123/utils/Solution.java
@@ -0,0 +1,51 @@
+package me.flo456123.utils;
+
+import me.flo456123.substance.Substance;
+import me.flo456123.substance.Unit;
+
+public class Solution {
+    private double amount;
+    private Substance substance;
+    private Unit unit;
+
+    /**
+     * Stores an amount of substance with a dedicated unit
+     * @param amount the amount of the substance
+     * @param substance the substance which is being stored
+     * @param unit the unit that the amount is in
+     */
+    public Solution(double amount, Substance substance, Unit unit) {
+        setAmount(amount);
+        setSubstance(substance);
+        setUnit(unit);
+    }
+
+    public Substance getSubstance() {
+        return substance;
+    }
+
+    private void setSubstance(Substance substance) {
+        this.substance = substance;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    private void setAmount(double amount) {
+        if (amount < 0) {
+            throw new UtilException("amount cannot be negative");
+        }
+
+        this.amount = amount;
+    }
+
+    public Unit getUnit() {
+        return unit;
+    }
+
+    private void setUnit(Unit unit) {
+        this.unit = unit;
+    }
+
+}
