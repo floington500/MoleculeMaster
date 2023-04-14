2cb45592d97236315d1a31f84b35a9c0c62383c3
flo456123 <zachappella@gmail.com>
2023-04-13 21:07:04 -0400

    added test for parser

---
+++ src/test/java/me/flo456123/common/utils/ParserTest.java
@@ -0,0 +1,46 @@
+package me.flo456123.common.utils;
+
+import me.flo456123.common.element.Element;
+import me.flo456123.common.element.ElementFactory;
+import me.flo456123.common.substance.Substance;
+import me.flo456123.common.substance.substances.Compound;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ParserTest {
+
+    @Test
+    public void validSubstanceTest1() {
+        Substance compound = Parser.parseSubstanceString("2HCl");
+
+        Element elementReactant1 = ElementFactory.createElement("H", 1);
+        Element elementReactant2 = ElementFactory.createElement("Cl", 1);
+        Compound compound1 = new Compound(2, elementReactant1, elementReactant2);
+
+        assertEquals(compound, compound1);
+    }
+
+    @Test
+    public void validSubstanceTest2() {
+        Substance compound = Parser.parseSubstanceString("2NaCl_2");
+
+        Element elementReactant1 = ElementFactory.createElement("Na", 1);
+        Element elementReactant2 = ElementFactory.createElement("Cl", 2);
+        Compound compound1 = new Compound(2, elementReactant1, elementReactant2);
+
+        assertEquals(compound, compound1);
+    }
+
+    @Test
+    public void validSubstanceTest3() {
+        Substance compound = Parser.parseSubstanceString("HS_4");
+
+        Element elementProduct3 = ElementFactory.createElement("H", 1);
+        Element elementProduct4 = ElementFactory.createElement("S", 4);
+        Compound compound1 = new Compound(1, elementProduct3, elementProduct4);
+
+        assertEquals(compound, compound1);
+    }
+
+}
