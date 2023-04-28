813775c76119dd3878ff6cf50d66ba73db70de1e
zacha <zachappella@gmail.com>
2023-04-27 21:31:25 -0400

    refactored substance to a record

--- src/main/java/com/github/flo456123/common/types/Substance.java
+++ src/main/java/com/github/flo456123/common/types/Substance.java
@@ -7,63 +7,7 @@ import java.util.Arrays;
 /**
  * This class represents a substance with a given number of moles and elements.
  */
-public class Substance
-{
-    private final double n;
-    private final Element[] elements;
-
-    /**
-     * Constructs a new {@link Substance} object with the given number of moles and elements.
-     *
-     * @param n        the number of moles
-     * @param elements the elements of the substance
-     */
-    public Substance(double n, Element... elements) {
-        this.n = n;
-        this.elements = elements;
-    }
-
-    public Element[] getElements() {
-        return elements;
-    }
-
-    public double getN() {
-        return n;
-    }
-
-    /**
-     * Returns the hash code for this Substance object.
-     *
-     * @return an int representing the hash code for this Substance object
-     */
-    @Override
-    public int hashCode() {
-        int result = 17;
-        long nBits = Double.doubleToLongBits(n);
-        result = 31 * result + (int) (nBits ^ (nBits >>> 32));
-        result = 31 * result + Arrays.hashCode(elements);
-        return result;
-    }
-
-    /**
-     * Returns true if this {@link Substance} object is equal to the specified object.
-     *
-     * @param obj the object to compare this Substance object against
-     * @return true if this {@link Substance} object is equal to the specified object, false otherwise
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-        Substance other = (Substance) obj;
-
-        return hashCode() == other.hashCode();
-    }
-
+public record Substance(int n, Element... elements) {
     /**
      * Returns a string representation of this {@link Substance} object.
      *
